225ff46996fbf19cbdcf3f47f6883c958774b99c
A-fandino <arnauf8@gmail.com>
2025-04-20 01:38:58 +0200

    Add statements

--- java/com/craftinginterpreters/lox/Expr.java
+++ java/com/craftinginterpreters/lox/Expr.java
@@ -4,34 +4,27 @@ import java.util.List;
 
 abstract class Expr {
   interface Visitor<R> {
-    // R visitTernaryExpr(Ternary expr);
+    R visitAssignExpr(Assign expr);
     R visitBinaryExpr(Binary expr);
     R visitGroupingExpr(Grouping expr);
     R visitLiteralExpr(Literal expr);
     R visitUnaryExpr(Unary expr);
+    R visitVariableExpr(Variable expr);
   }
-  static class Ternary extends Expr {
-    Ternary(Expr left, Token firstOperator, Expr mid, Token secondOperator, Expr right) {
-      this.left = left;
-      this.firstOperator = firstOperator;
-      this.mid = mid;
-      this.secondOperator = secondOperator;
-      this.right = right;
+  static class Assign extends Expr {
+    Assign(Token name, Expr value) {
+      this.name = name;
+      this.value = value;
     }
 
-    // @Override
-    // <R> R accept(Visitor<R> visitor) {
-    //   return visitor.visitTernaryExpr(this);
-    // }
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitAssignExpr(this);
+    }
 
-    final Expr left;
-    final Token firstOperator;
-    final Expr mid;
-    final Token secondOperator;
-    final Expr right;
+    final Token name;
+    final Expr value;
   }
-
-
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
@@ -85,6 +78,18 @@ abstract class Expr {
 
     final Token operator;
     final Expr right;
+  }
+  static class Variable extends Expr {
+    Variable(Token name) {
+      this.name = name;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVariableExpr(this);
+    }
+
+    final Token name;
   }
 
  abstract <R> R accept(Visitor<R> visitor);
